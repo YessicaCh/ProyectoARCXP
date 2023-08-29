1cea9edc5c5a184662715f4e7fe7990911da5ddb
luis_c_l <daprimovaria@gmail.com>
2023-08-29 17:27:32 -0500

    feat: agregando nuevos componentes "banner" y "tab"

--- ARC/components/features/banner/default.jsx
+++ ARC/components/features/banner/default.jsx
@@ -1,21 +1,31 @@
-import React from 'react';
-import './banner.css';
+import React from "react";
+import PropTypes from "prop-types";
+import "./banner.css";
 
-const Banner = () => {
-  return (
-    <div className="banner bg-dark text-center py-5">
-      <div className="container">
-        <h1 className="display-4 text-white">
-          Te explico cómo me hubiese gustado que me explicaran
-        </h1>
-      </div>
-    </div>
-  );
+const Banner = (props) => {
+    const { customFields } = props;
+    const { contentBanner } = customFields;
+
+    return (
+        <div className="banner bg-dark text-center py-5">
+            <div className="container">
+                <h1 className="display-4 text-white">{contentBanner}</h1>
+            </div>
+        </div>
+    );
 };
 
 Banner.label = {
-  en: 'Banner',
-  es: 'Banner'
+    en: "Banner",
+    es: "Banner",
+};
+
+Banner.propTypes = {
+    customFields: PropTypes.shape({
+        contentBanner: PropTypes.string.tag({
+            name: "Contenido",
+        }),
+    }),
 };
 
 export default Banner;
--- ARC/components/features/tab/default.jsx
+++ ARC/components/features/tab/default.jsx
@@ -1,52 +1,87 @@
-import React, { useState } from 'react';
+import React, { useState } from "react";
+import PropTypes from "prop-types";
 // import './TabComponent.css'; // Agrega tus estilos CSS aquí
 
-const TabComponent = () => {
-  const [activeTab, setActiveTab] = useState('perfil'); // Estado para controlar la pestaña activa
-  const tabs = ['perfil', 'experiencia']; // Nombres de las pestañas
+const TabComponent = (props) => {
+    const { customFields } = props;
+    const { tab01, tab02, contentTab01, contentTab02 } = customFields;
+    const [activeTab, setActiveTab] = useState("perfil"); // Estado para controlar la pestaña activa
+    // const tabs = ["perfil", "experiencia"]; // Nombres de las pestañas
+    const tabs = [
+        { name: tab01 || "Tab 01", content: contentTab01 },
+        { name: tab02 || "Tab 02", content: contentTab02 },
+    ];
+    const handleTabClick = (tab) => {
+        setActiveTab(tab);
+    };
 
-  const handleTabClick = (tab) => {
-    setActiveTab(tab);
-  };
-
-  return (
-    <div className="tab-component px-5">
-      <ul className="nav nav-tabs">
-        {tabs.map((tab) => (
-          <li className="nav-item" key={tab}>
-            <a
-              className={`nav-link ${activeTab === tab ? 'active' : ''}`}
-              onClick={() => handleTabClick(tab)}
-              href="#"
-            >
-              {tab.charAt(0).toUpperCase() + tab.slice(1)}
-            </a>
-          </li>
-        ))}
-      </ul>
-      <div className="tab-content">
-        {activeTab === 'perfil' && (
-          <div className="tab-pane active">
-            {/* Contenido para la pestaña de perfil */}
-            <h3>Información de Perfil</h3>
-            {/* Agrega aquí la información de perfil */}
-          </div>
-        )}
-        {activeTab === 'experiencia' && (
-          <div className="tab-pane">
-            {/* Contenido para la pestaña de experiencia */}
-            <h3>Experiencia Laboral</h3>
-            {/* Agrega aquí la información de experiencia laboral */}
-          </div>
-        )}
-      </div>
-    </div>
-  );
+    return (
+        <div className="tab-component px-5">
+            <ul className="nav nav-tabs">
+                {tabs?.length &&
+                    tabs?.map((tab) => (
+                        <li className="nav-item" key={tab?.name}>
+                            <a
+                                className={`nav-link ${
+                                    activeTab === tab?.name ? "active" : ""
+                                }`}
+                                onClick={() => handleTabClick(tab?.name)}
+                                href="#"
+                            >
+                                {tab?.name?.charAt(0).toUpperCase() +
+                                    tab?.name?.slice(1)}
+                            </a>
+                        </li>
+                    ))}
+            </ul>
+            <div className="tab-content">
+                {/* {activeTab === "perfil" && (
+                    <div className="tab-pane active">
+                        <h3>Información de Perfil</h3>
+                    </div>
+                )}
+                {activeTab === "experiencia" && (
+                    <div className="tab-pane">
+                        <h3>Experiencia Laboral</h3>
+                    </div>
+                )} */}
+                {tabs?.length &&
+                    tabs?.map(
+                        (tab) =>
+                            activeTab === tab.name && (
+                                <div className="tab-pane active">
+                                    <h3>{tab.content}</h3>
+                                </div>
+                            )
+                    )}
+            </div>
+        </div>
+    );
 };
 
 TabComponent.label = {
-    en: 'TabComponent',
-    es: 'Tab component'
-  };
-  
+    en: "TabComponent",
+    es: "Tab component",
+};
+
+TabComponent.propTypes = {
+    customFields: PropTypes.shape({
+        tab01: PropTypes.string.tag({
+            name: "Nombre del tab 01",
+            group: "Tab 01",
+        }),
+        contentTab01: PropTypes.text.tag({
+            name: "Contenido del tab 01",
+            group: "Tab 01",
+        }),
+        tab02: PropTypes.string.tag({
+            name: "Nombre del tab 02",
+            group: "Tab 02",
+        }),
+        contentTab02: PropTypes.text.tag({
+            name: "Contenido del tab 02",
+            group: "Tab 02",
+        }),
+    }),
+};
 export default TabComponent;
